6af89e08d6c32c1a6a96efb4d553b7ba7ebfd467
Henry Ou <henryou@henryou.com>
2018-04-28 21:36:36 -0700

    Rudimentary Comparitor Implementation Completed

--- pic10b_vector.h
+++ pic10b_vector.h
@@ -83,9 +83,17 @@ namespace Pic10b{
         T& operator[]( size_t index ){
             return the_data[index];
         }
-        
+
         T operator[]( size_t index ) const {
             return the_data[index];
+        }
+
+        bool operator<(const vector<T>& a) const {
+            return a.squared_norm() > squared_norm();
+        }
+
+        bool operator==(const vector<T>& a) const {
+            return  a.squared_norm() == squared_norm();
         }
 
         void dump_data_to( std::ostream& out ) const {
@@ -109,6 +117,14 @@ namespace Pic10b{
         void pop_back(){
             if ( the_size > 0 )
                 --the_size;
+        }
+
+        T squared_norm() const{
+            T norm = 0;
+            for (int i = 0; i < the_size; ++i){
+                norm += (this->operator[](i))*(this->operator[](i));
+            }
+            return norm;
         }
 
       private:
@@ -150,31 +166,31 @@ void print_vector( const Pic10b::vector<T>& v ){
 } 
 
 template <class T>
-Integer operator+(Integer lhs, const Integer& rhs) {
+T operator+(T lhs, const T& rhs) {
 	return lhs += rhs;
 }
 
 template <class T>
-Integer operator*(Integer lhs, const Integer& rhs) {
+T operator*(T lhs, const T& rhs) {
 	return lhs *= rhs;
 }
 
 template <class T>
-bool operator>(const Integer& a, const Integer& b) {
+bool operator>(const T& a, const T& b) {
 	return (!(a < b) && !(a == b));
 }
 
 template <class T>
-bool operator<=(const Integer& a, const Integer& b) {
+bool operator<=(const T& a, const T& b) {
 	return (a < b || a == b);
 }
 
 template <class T>
-bool operator>=(const Integer& a, const Integer& b) {
+bool operator>=(const T& a, const T& b) {
 	return !(a < b);
 }
 
 template <class T>
-bool operator!=(const Integer& a, const Integer& b) {
+bool operator!=(const T& a, const T& b) {
 	return !(a == b);
 }
